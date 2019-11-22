b6883a370232e138dce3000f749586931db14d67
Claude Heyman <claude.heyman@ubisoft.com>
2019-11-22 15:10:28 +0100

    Initial commit from Spring Initializr base

---
+++ src/test/java/com/claudeheyman/adventofcode/ContextLoadsTest.java
@@ -0,0 +1,13 @@
+package com.claudeheyman.adventofcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContextLoadsTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
