7623040c9db831c799ee04414c3504fbfcd56519
Claude Heyman <claude.heyman@ubisoft.com>
2019-12-06 16:28:47 +0100

    refactor: solutionless

--- src/main/java/com/claudeheyman/adventofcode/service/SolutionService.java
+++ src/main/java/com/claudeheyman/adventofcode/service/SolutionService.java
@@ -2,7 +2,6 @@ package com.claudeheyman.adventofcode.service;
 
 import com.claudeheyman.adventofcode.service.solution.DaySolution;
 import com.claudeheyman.adventofcode.service.solution.NotYetDoneSolution;
-import com.claudeheyman.adventofcode.solved.Solved2018;
 import com.claudeheyman.adventofcode.solved.Solved2019;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,14 +11,10 @@ public class SolutionService {
 	private final static DaySolution NOT_COMPLETED = new NotYetDoneSolution();
 
 	@Autowired
-	private Solved2018 solutions2018;
-
-	@Autowired
 	private Solved2019 solutions2019;
 
 	public DaySolution getSolution(int year, int day) {
 		switch (year) {
-			case 2018 : return solutions2018.getSolution(day);
 			case 2019 : return solutions2019.getSolution(day);
 		}
 
--- src/main/java/com/claudeheyman/adventofcode/solved/Solved2019.java
+++ src/main/java/com/claudeheyman/adventofcode/solved/Solved2019.java
@@ -2,7 +2,7 @@ package com.claudeheyman.adventofcode.solved;
 
 import com.claudeheyman.adventofcode.service.solution.DaySolution;
 import com.claudeheyman.adventofcode.service.solution.YearSolution;
-import com.claudeheyman.adventofcode.solved.year2019.Solver2019Day1;
+import com.claudeheyman.adventofcode.solved.year2019.SolverDayExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,16 +10,12 @@ import org.springframework.stereotype.Component;
 public class Solved2019 implements YearSolution {
 
 	@Autowired
-	private Solver2019Day1 day1;
-	@Autowired
-	private Solver2019Day1 day2;
+	private SolverDayExample daySolver;
 
 	@Override
 	public DaySolution getSolution(int day) {
 		switch (day) {
-			case 1: return day1;
-			case 2: return day2;
-			default: return null;
+			default: return daySolver;
 		}
 	}
 }
